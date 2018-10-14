093df1da2046fda20bfd14aaa9dbb3a488e2de22
Angelica Costantini <angelicacostantini07@gmail.com>
2018-10-14 15:20:19 +0200

    adding spec file for about page, test pass

---
+++ src/pages/about/about.spec.ts
@@ -0,0 +1,35 @@
+import { AboutPage } from "./about";
+import { TestBed, async } from "@angular/core/testing";
+import { IonicModule, Platform, NavController } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { PlatformMock, StatusBarMock, SplashScreenMock, NavControllerMock } from "ionic-mocks";
+
+describe("About", () => {
+  let aboutpage;
+  let fixture;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AboutPage
+      ],
+      imports: [IonicModule.forRoot(AboutPage)],
+      providers: [
+        { provide: Platform, useFactory: () => PlatformMock.instance() },
+        { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
+        { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
+        { provide: NavController, useFactory: () => NavControllerMock.instance() }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutPage);
+    aboutpage = fixture.componentInstance;
+  });
+
+  it("should create the about page", () => {
+    expect(aboutpage).toBeTruthy();
+    expect(aboutpage instanceof AboutPage).toEqual(true);
+  });
